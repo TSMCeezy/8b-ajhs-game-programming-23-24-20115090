ce5870fdabdaa34eee683337bc9eb2e2d8fa3b27
20115090 <s20115090@students.duvalschools.org>
2023-10-04 14:32:57 -0400

    v0.1c
    Accessing first and last elements in a list.

--- gameProgramming/computer_science_exercises/02_ collections/collections.py
+++ gameProgramming/computer_science_exercises/02_ collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Jordan Hnery v0.1b
+# Collections Examples, Jordan Hnery v0.1c
 
 # LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES
 breakfastFoods = ["Bacon", "Waffles"," Pancakes", " Cereal", "Milk"]
@@ -10,6 +10,16 @@ testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1.74,1.99, 0.99, 4.25]
 
 # Printing Contents of an list 
-print(breakfastFoods)
-print(testScores)
-print(classGPA)
+#print(breakfastFoods)
+#print(testScores)
+#print(classGPA) 
+
+# Acessing Specific List Elements -- REMEBER FIRST ELEMENT IS INDEX 0
+#print(breakfastFoods[0])
+#print(testScores[0])
+#print(classGPA[0]) 
+
+# Acessing Last Element in Lists
+#print(breakfastFoods[-1])
+#print(testScores[-1])
+#print(classGPA[-1]) 
