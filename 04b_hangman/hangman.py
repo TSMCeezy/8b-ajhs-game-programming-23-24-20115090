45fd308ed104c33f10000a922968173305d56d2b
20115090 <s20115090@students.duvalschools.org>
2023-10-31 14:17:34 -0400

    v2.2
    hangman loop

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -1,4 +1,39 @@
 # Hangman game by Jordan Henry , v0.0
 
 words = 'cat dog hat pat rat saw sad bed tab pet school uniform microsoft program operate function collection instructions license hangman ironic nauseous coincidental phenomenom onomatopoeia necessary appealing seperate platonic electrons'.split()
-print(words)
+
+# VARIABLE_NAMES in all CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+    +---+
+        |
+        |
+        |
+    =======''','''
+    +---+
+     O  |
+        |
+        |
+    =======''','''
+    +---+
+     O  |
+     |  |
+        |
+    =======''','''
+    +---+
+     O  |
+    /|  |
+        |
+    =======''','''
+    +---+
+     O  |
+    /|\ |
+        |
+    =======''','''
+    +---+
+     O  |
+    /|\ |
+    /\  |
+    =======''']
+ 
+
+
