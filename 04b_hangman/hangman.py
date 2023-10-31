33dcc8adccf46f4f08003614870b5357af209555
20115090 <s20115090@students.duvalschools.org>
2023-10-31 13:58:17 -0400

    v1.1
    word list

--- 04b_hangman/hangman.py
+++ 04b_hangman/hangman.py
@@ -1,1 +1,4 @@
 # Hangman game by Jordan Henry , v0.0
+
+words = 'cat dog hat pat rat saw sad bed tab pet school uniform microsoft program operate function collection instructions license hangman ironic nauseous coincidental phenomenom onomatopoeia necessary appealing seperate platonic electrons'.split()
+print(words)
