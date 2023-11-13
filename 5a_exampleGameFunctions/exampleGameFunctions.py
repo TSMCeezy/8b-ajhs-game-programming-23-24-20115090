9699e81969661255295f6eca69944f4669ac23a7
20115090 <s20115090@students.duvalschools.org>
2023-11-13 13:51:04 -0500

    v10.0
    four functions

--- 5a_exampleGameFunctions/exampleGameFunctions.py
+++ 5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,14 @@
 # Exxample Game Functions Project, Jordan Henry v10.0
+import random 
+
+def functionOne():
+    pass 
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default  Value"):
+    print(param1)
+
+def FunctionFour(param1, param2, param3):
+    pass
